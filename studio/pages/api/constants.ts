8aca056ba414acfa2945d5c765c4944873d0e607
Enoch Enchill <et.enchill@gmail.com>
2023-03-28 05:40:41 +0000

    figuring out multiprojects

--- studio/pages/api/constants.ts
+++ studio/pages/api/constants.ts
@@ -4,6 +4,22 @@ export const PROJECT_REST_URL = `${PUBLIC_URL.origin}/rest/v1/`
 export const PROJECT_ENDPOINT = PUBLIC_URL.host
 export const PROJECT_ENDPOINT_PROTOCOL = PUBLIC_URL.protocol.replace(':', '')
 
+var project_names = process.env.DEFAULT_PROJECT_NAMES?.split(",")
+var project_configs: { id: number; ref: string; name: string; organization_id: number; cloud_provider: string; status: string; region: string; inserted_at: string }[] = []
+project_names?.forEach((name, index) => {
+  const id = 1 + index
+  const ref = name.toLowerCase()
+  project_configs.push({
+    id: id,
+    ref: ref,
+    name: name,
+    organization_id: 1,
+    cloud_provider: 'localhost',
+    status: 'ACTIVE_HEALTHY',
+    region: 'local',
+    inserted_at: '2021-08-02T06:40:40.646Z',
+  })
+})
 export const DEFAULT_PROJECT = {
   id: 1,
   ref: 'default',
@@ -14,3 +30,5 @@ export const DEFAULT_PROJECT = {
   region: 'local',
   inserted_at: '2021-08-02T06:40:40.646Z',
 }
+
+export const DEFAULT_PROJECTS = project_configs
